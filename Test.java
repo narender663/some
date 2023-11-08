84247a0366bb8a6b37fdc784bb86ec8a0f42cd43
narender663 <150145884+narender663@users.noreply.github.com>
2023-11-08 18:23:41 +0530

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,12 @@
+ class Test
+{
+  public static void main(String []args)
+   {
+      String str="Behappy";
+     int cnt=2;
+     String res1=str.substring(0,cnt);
+     
+     String res2=str.substring(cnt,str.length());
+      System.out.println(res2+ res1);
+    }
+ }
